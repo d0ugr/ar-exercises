b1ecb33a8f1143d7cdaa9840af611067f71f3914
d0ugr <doug@escm.ml>
2020-04-30 23:01:18 -0400

    Exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -2,8 +2,40 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
+require 'pp'
 
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+store = Store.new(
+  name:           "Surrey",
+  annual_revenue: 224000,
+  mens_apparel:   false,
+  womens_apparel: true
+)
+store.save
+store = Store.new(
+  name:           "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel:   true,
+  womens_apparel: false
+)
+store.save
+store = Store.new(
+  name:           "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel:   true,
+  womens_apparel: true
+)
+store.save
+puts "Store count: #{Store.count}"
+puts "Men's apparel"
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each{ |store|
+  puts "#{store.name} brings in #{store.annual_revenue} per year"
+}
+puts "Women's apparel"
+@womens_stores = Store.where(["womens_apparel = ? and annual_revenue < ?", true, 1000000])
+@womens_stores.each{ |store|
+  puts "#{store.name} brings in #{store.annual_revenue} per year"
+}
